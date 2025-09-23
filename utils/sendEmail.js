ce59000a284ee72eb49a16f6e9b7299f46cdf605
asifsaascraft <asifsaascraft@gmail.com>
2025-09-24 00:47:39 +0530

    Add console in sendEmail file

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,14 +8,16 @@ const sendEmail = async (options) => {
       pass: process.env.EMAIL_PASS
     }
   });
-
+  console.log("Email transporter created", transporter);
   const mailOptions = {
     from: `"AIG Event" <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
     html: options.message
   };
-
+  console.log("Sending email to:", options.email);
+  console.log("Email subject:", options.subject);
+  console.log("Email message:", options.message);
   await transporter.sendMail(mailOptions);
 };
 
