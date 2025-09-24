70aaa686beb02a3d10b491f0dddffa52678afafa
Asif <asifsaascraft@gmail.com>
2025-09-24 12:55:41 +0530

    Again Update sendEmail code

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,44 +1,29 @@
-import nodemailer from "nodemailer";
 import { google } from "googleapis";
 
 const sendEmail = async (options) => {
-  try {
-    const oAuth2Client = new google.auth.OAuth2(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      "https://developers.google.com/oauthplayground" // redirect URI
-    );
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
 
-    oAuth2Client.setCredentials({
-      refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-    });
+  oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
 
-    // Get access token using refresh token
-    const accessToken = await oAuth2Client.getAccessToken();
+  const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-        accessToken: accessToken.token, // important
-      },
-    });
+  const rawMessage =
+    `From: "AIG Event" <${process.env.EMAIL_USER}>\r\n` +
+    `To: ${options.email}\r\n` +
+    `Subject: ${options.subject}\r\n\r\n` +
+    `${options.message}`;
 
-    const mailOptions = {
-      from: `"AIG Event" <${process.env.EMAIL_USER}>`,
-      to: options.email,
-      subject: options.subject,
-      html: options.message,
-    };
-    await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.error("Error sending email :", error);
-    throw error;
-  }
+  const encodedMessage = Buffer.from(rawMessage).toString("base64").replace(/\+/g, "-").replace(/\//g, "_");
+
+  await gmail.users.messages.send({
+    userId: "me",
+    requestBody: { raw: encodedMessage },
+  });
+
 };
 
 export default sendEmail;
